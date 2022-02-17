a5f7981cdc35af1aa59e05bc0b9887d3764a8733
suruimeng.srm <suruimeng.srm@antgroup.com>
2022-02-17 16:32:05 +0800

    111

--- src/toy.ts
+++ src/toy.ts
@@ -1,3 +1,12 @@
-const  num1:  number = 1;
+const canvas = document.body.appendChild(document.createElement('canvas'));
+  // 初始化 GL context
+  const gl = canvas.getContext("webgl");
+  // 當 WebGL 有效才繼續執行
+  if (gl === null) {
+    alert("無法初始化 WebGL，您的瀏覽器不支援。");
+  }
 
-console.log(num1);
+  // 設定清除色彩為黑色，完全不透明
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // 透過清除色來清除色彩緩衝區
+  gl.clear(gl.COLOR_BUFFER_BIT);
